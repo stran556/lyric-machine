a1c74f8a151a09e6fce2488772068351d482def6
Sean <36829049+stran556@users.noreply.github.com>
2022-11-05 00:05:29 -0500

    Add files via upload

---
+++ lyricmachine/view.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+track=$1
+artist=$2
+
+w3m -dump $(./search.sh 'genius lyrics '$artist' '$track | grep -m 1 lyrics | sed 's|\(.*\)lyrics.*|\1|')'lyrics' >> $(pwd)/lyrics.txt
+
+cat lyrics.txt | grep -A 1000 Lyrics | sed -n '/Embed/q;p'
+
+: > lyrics.txt
