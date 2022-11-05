0e85e50fd2be7185c77c095f001bd464a8ef919d
Sean <36829049+stran556@users.noreply.github.com>
2022-11-05 00:37:03 -0500

    Add files via upload

---
+++ lyricmachine/track_list.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+settings=$1
+url=$2
+
+if [ "$settings" == "-a" ];
+then
+	./import_list.sh $url > /dev/null 2>&1
+
+	# GET PLAYLIST TITLE
+	cat $(pwd)/content.txt | grep -o -P 'title'\>'.{0,75}' | grep ' - ' | cut -c 6- | sed -e 's/.*'\>'\(.*\) - .*/\1/' >> $(pwd)/data.txt 2>&1
+
+	# GET PLAYLIST OWNER
+	cat $(pwd)/content.txt | grep -o -P 'playlist by.{0,75}' | cut -c 10- | sed -e 's/.*by \(.*\) | .*/\1/' >> $(pwd)/data.txt 2>&1
+
+	# GET TOTAL DURATION
+	cat $(pwd)/content.txt | grep -o -P '.{0,100} min' | sed -e 's/.*'\>'\(.*\).*/\1/' >> $(pwd)/data.txt 2>&1
+	cat $(pwd)/content.txt | grep -o -P 'over 24 hr' >> $(pwd)/data.txt 2>&1
+
+	# GET TRACKS
+	cat $(pwd)/content.txt | grep -o -P 'track .{0,100}' | grep -v '</a>' | sed -e 's/.*track \(.*\)\" .*/\1/' >> $(pwd)/data.txt 2>&1
+
+	echo '' >> $(pwd)/data.txt 2>&1
+
+	# GET ARTISTS
+	cat $(pwd)/content.txt | grep -o -P 'artist.{0,200}' | cut -c 31- | rev | cut -c 125- | rev | grep '<' | sed -e 's/.*'\>'\(.*\)'\<'\/a'\>'.*/\1/' >> $(pwd)/data.txt 2>&1
+	echo " --" >> $(pwd)/data.txt 2>&1
+
+	: > $(pwd)/content.txt
+else
+	python3 main.py
+fi
--- lyricmachine/view.sh
+++ lyricmachine/view.sh
@@ -5,6 +5,7 @@ artist=$2
 
 w3m -dump $(./search.sh 'genius lyrics '$artist' '$track | grep -m 1 lyrics | sed 's|\(.*\)lyrics.*|\1|')'lyrics' >> $(pwd)/lyrics.txt
 
-cat lyrics.txt | grep -A 1000 Lyrics | sed -n '/Embed/q;p'
+cat lyrics.txt | grep -A 1000 Lyrics | sed -n '/Embed/q;p' | sed -n '/You might also like/q;p'
 
 : > lyrics.txt
+
