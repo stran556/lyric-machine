974a0f2877ec2082084905ba600818e4be11e27f
Sean <36829049+stran556@users.noreply.github.com>
2022-11-11 22:07:25 -0600

    Add files via upload

---
+++ lyricmachine/remove.py
@@ -0,0 +1,26 @@
+import os
+file = open("data.txt", "r")
+
+playlist_names = []
+
+counter = 0
+for line in file:
+    if counter == 0:
+        playlist_names.append(line.strip())
+    if line.strip() == "--":
+        counter = 0
+    else:
+        counter = counter + 1
+print()
+os.system('echo remove playlist')
+print()
+print("#  -PLAYLIST-")
+i = 1
+for a in playlist_names:
+    print(str(i) + "  " + a)
+    i = i + 1
+
+
+
+os.system('echo ' + str(i - 1) + ' >&1 >> content.txt')
+os.system('python3 print.py')
