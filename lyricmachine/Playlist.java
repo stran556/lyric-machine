00104f7d38af76a55e73066903614ce38fd10a2d
Sean <36829049+stran556@users.noreply.github.com>
2022-11-15 23:07:41 -0600

    Add files via upload

---
+++ lyricmachine/Playlist.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    String title;
+    String owner;
+    String duration;
+    ArrayList<String> trackList;
+    ArrayList<String> artistList;
+
+    public Playlist(){
+
+    }
+
+    public Playlist(String title, String owner, String duration, ArrayList<String> trackList, ArrayList<String> artistList){
+        this.title = title;
+        this.owner = owner;
+        this.duration = duration;
+        this.trackList = trackList;
+        this.artistList = artistList;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getOwner(){
+        return owner;
+    }
+    public String getDuration(){
+        return duration;
+    }
+    public ArrayList<String> getTrackList(){
+        return trackList;
+    }
+    public ArrayList<String> getArtistList(){
+        return artistList;
+    }
+
+
+
+
+
+
+}
