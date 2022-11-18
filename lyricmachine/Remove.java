82125a02169c50226a4e568babc697dc42f8d5d7
Sean <36829049+stran556@users.noreply.github.com>
2022-11-17 19:30:57 -0600

    Add files via upload

--- lyricmachine/Remove.java
+++ lyricmachine/Remove.java
@@ -5,21 +5,53 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.FileWriter;
 
 public class Remove {
 
-    public static void updateFile(){
+    public static void updateList(ArrayList<Playlist> ml) throws InterruptedException{
         
+        File data = new File("data.txt");
+        
+        try {
+            FileWriter writer = new FileWriter(data);
+            for(int i = 0; i < ml.size(); i++){
+                writer.write(ml.get(i).getTitle() + "\n");
+                writer.write(ml.get(i).getOwner() + "\n");
+                writer.write(ml.get(i).getDuration() + "\n");
+
+                for(int x = 0; x < ml.get(i).getTrackList().size(); x++){
+                    writer.write(ml.get(i).getTrackList().get(x) + "\n");
+                }
+                writer.write("=+-+=\n"); //Track delim
+
+                for(int y = 0; y < ml.get(i).getArtistList().size(); y++){
+                    writer.write(ml.get(i).getArtistList().get(y) + "\n");
+                }
+                writer.write("=-+-=\n"); //Artist delim
+            }
+            
+            writer.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+          
     }
 
-    public static void printMainScreen(ArrayList<Playlist> ml){
+    public static void printMainScreen(ArrayList<Playlist> ml) throws InterruptedException{
+        Main.linuxCommand("clear");
         System.out.println("[remove playlist]\n");
         System.out.println("#  -PLAYLIST-");
         for(int i = 0; i < ml.size(); i++){
             System.out.println((i + 1) + "  " + ml.get(i).getTitle());
         }
-        System.out.println("0  [EXIT]");
+
+        Thread.sleep(20);
+        System.out.println("\n0  [EXIT]");
         
+        Thread.sleep(20);
+        System.out.print("\nEnter # (delete): ");
+
         Scanner s = new Scanner(System.in);
         int input = s.nextInt();
 
@@ -30,7 +62,7 @@ public class Remove {
 
         if(input != 0){
             ml.remove(input - 1);
-
+            updateList(ml);
 
         }
         else{
@@ -38,10 +70,11 @@ public class Remove {
         }
 
     }
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException{
 
         ArrayList<Playlist> mainList = Main.getPlaylists();
 
+        Main.linuxCommand("clear");
         printMainScreen(mainList);
 
     }
