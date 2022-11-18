eb75525148d66212671a382b5f6a75662965c51a
Sean <36829049+stran556@users.noreply.github.com>
2022-11-18 00:08:24 -0600

    Add files via upload

--- lyricmachine/Remove.java
+++ lyricmachine/Remove.java
@@ -8,15 +8,17 @@ public class Remove {
     public static void printMainScreen(ArrayList<Playlist> ml) throws InterruptedException{
         Main.linuxCommand("clear");
         System.out.println("[remove playlist]\n");
+        Thread.sleep(50);
         System.out.println("#  -PLAYLIST-");
         for(int i = 0; i < ml.size(); i++){
+            Thread.sleep(50);
             System.out.println((i + 1) + "  " + ml.get(i).getTitle());
         }
 
-        Thread.sleep(20);
+        Thread.sleep(50);
         System.out.println("\n0  [EXIT]");
         
-        Thread.sleep(20);
+        Thread.sleep(50);
         System.out.print("\nEnter # (delete): ");
 
         Scanner s = new Scanner(System.in);
@@ -31,6 +33,9 @@ public class Remove {
             ml.remove(input - 1);
             Main.updateList(ml);
 
+            Main.linuxCommand("clear");
+            System.out.println("Playlist deleted.");
+            Thread.sleep(1000);
         }
         else{
             System.exit(0);
