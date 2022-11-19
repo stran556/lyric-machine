9b7cfead0610a2cc1f1fc6a2a514ff68a92981d5
Sean <36829049+stran556@users.noreply.github.com>
2022-11-19 11:36:28 -0600

    Add files via upload

--- lyricmachine/track_list.sh
+++ lyricmachine/track_list.sh
@@ -15,30 +15,40 @@ then
 elif [ "$settings" == "-a" ];
 then
 	url=$2
-	if [[ "$url" == *"open.spotify.com/playlist"* ]];
+	if [[ "$url" == *"open.spotify.com/playlist/"* ]];
 	then
 		./import_list.sh $url > /dev/null 2>&1
 
 		# GET PLAYLIST TITLE
 		cat $(pwd)/content.txt | grep -o -P 'title'\>'.{0,75}' | grep ' - ' | cut -c 6- | sed -e 's/.*'\>'\(.*\) - .*/\1/' >> $(pwd)/data.txt 2>&1
 
-		# GET PLAYLIST OWNER
-		cat $(pwd)/content.txt | grep -o -P 'playlist by.{0,75}' | cut -c 10- | sed -e 's/.*by \(.*\) | .*/\1/' >> $(pwd)/data.txt 2>&1
+		string=$(cat content.txt | tr -d "[:blank:]")
 
-		# GET TOTAL DURATION
-		cat $(pwd)/content.txt | grep -o -P '.{0,100} min' | sed -e 's/.*'\>'\(.*\).*/\1/' >> $(pwd)/data.txt 2>&1
-		cat $(pwd)/content.txt | grep -o -P 'over 24 hr' >> $(pwd)/data.txt 2>&1
+		if [[ ! -z "$string" ]];
+		then
+			# GET PLAYLIST OWNER
+			cat $(pwd)/content.txt | grep -o -P 'playlist by.{0,75}' | cut -c 10- | sed -e 's/.*by \(.*\) | .*/\1/' >> $(pwd)/data.txt 2>&1
 
-		# GET TRACKS
-		cat $(pwd)/content.txt | grep -o -P 'track .{0,100}' | grep -v '</a>' | sed -e 's/.*track \(.*\)\" .*/\1/' >> $(pwd)/data.txt 2>&1
+			# GET TOTAL DURATION
+			cat $(pwd)/content.txt | grep -o -P '.{0,100} min' | sed -e 's/.*'\>'\(.*\).*/\1/' >> $(pwd)/data.txt 2>&1
+			cat $(pwd)/content.txt | grep -o -P 'over 24 hr' >> $(pwd)/data.txt 2>&1
 
-		echo '=+-+=' >> $(pwd)/data.txt 2>&1
+			# GET TRACKS
+			cat $(pwd)/content.txt | grep -o -P 'track .{0,100}' | grep -v '</a>' | sed -e 's/.*track \(.*\)\" .*/\1/' >> $(pwd)/data.txt 2>&1
 
-		# GET ARTISTS
-		cat $(pwd)/content.txt | grep -o -P 'artist.{0,200}' | cut -c 31- | rev | cut -c 125- | rev | grep '<' | sed -e 's/.*'\>'\(.*\)'\<'\/a'\>'.*/\1/' >> $(pwd)/data.txt 2>&1
-		echo "=-+-=" >> $(pwd)/data.txt 2>&1
+			echo '=+-+=' >> $(pwd)/data.txt 2>&1
 
-		: > $(pwd)/content.txt
+			# GET ARTISTS
+			cat $(pwd)/content.txt | grep -o -P 'artist.{0,200}' | cut -c 31- | rev | cut -c 125- | rev | grep '<' | sed -e 's/.*'\>'\(.*\)'\<'\/a'\>'.*/\1/' >> $(pwd)/data.txt 2>&1
+			echo "=-+-=" >> $(pwd)/data.txt 2>&1
+
+			: > $(pwd)/content.txt
+			echo "Playlist added."
+		else
+			echo "Invalid link. No changes made."
+		fi
+	else
+		echo "Invalid link. No changes made."
 	fi
 
 elif [ "$settings" == "-s" ];
